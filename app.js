ec800ce27a1d402d58378430ed6e65bed6dcf98c
Nur Akmal <mdnurakmal@gmail.com>
2022-01-08 23:26:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -59,21 +59,10 @@ function calculateDistance(origins,des){
         return console.log('no distances');
     }
 
-    if (distances.status == 'OK') {
-      for (var i=0; i < origins.length; i++) {
-          for (var j = 0; j < destinations.length; j++) {
-              var origin = distances.origin_addresses[i];
-              var destination = distances.destination_addresses[j];
-              if (distances.rows[0].elements[j].status == 'OK') {
-                  var distance = distances.rows[i].elements[j].distance.text;
-                  console.log('Distance from ' + origin + ' to ' + destination + ' is ' + distance);
-              } else {
-                  console.log(destination + ' is not reachable by land from ' + origin);
-              }
-          }
-      }
-  }
-  })
+    if (!err)
+        console.log("From : " + origins + " To : " + des + " : " + distances[0]);
+    
+  });
 }
 
 router.get('/rate', (request, response) => {
