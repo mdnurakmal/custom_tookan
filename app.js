9843be9f8c52850c46dff7892a5eb84e6239a15f
Nur Akmal <mdnurakmal@gmail.com>
2022-01-09 01:23:24 +0800

    Update app.js

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ function calculateDistance(origins,destinations){
               if (distances.rows[0].elements[j].status == 'OK') {
                   var distance = distances.rows[i].elements[j].distance.text;
                   console.log('Distance from ' + origin + ' to ' + destination + ' is ' + distance);
-                  calculatedDistance[j].push({j:distance.split(" ")[0]})
+                  calculatedDistance.push({j:distance.split(" ")[0]});
               } else {
                   //console.log(destination + ' is not reachable by land from ' + origin);
               }
