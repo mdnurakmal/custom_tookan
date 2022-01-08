dae9ff342ae324384ed40bc7580d71c79da0e171
Nur Akmal <mdnurakmal@gmail.com>
2022-01-08 16:41:15 +0800

    Update app.js

--- app.js
+++ app.js
@@ -112,11 +112,11 @@ router.post('/edit_order',(request,response) => {
         "has_pickup": "1",
         "has_delivery": "1",
         "layout_type": "0",
-        "job_pickup_datetime":"2022-01-08 23:24:00",
-        "job_delivery_datetime":"2022-01-08 23:24:00",
-        "job_pickup_address":"he11llo",
-        "customer_address": "hey1",
-        "job_id":"353609640"
+        "time":"2022-01-08 23:24:00",
+
+        "address":"xxx",
+
+        "job_id":"353609637"
 
       })
       .then(res => {
