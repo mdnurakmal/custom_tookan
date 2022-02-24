49453a01170de547e307b41c1eeb6dd5141f8a1e
Nur Akmal <2183322C@student.tp.edu.sg>
2022-02-24 14:46:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ var fs = require('fs');
 
 var https_options = {
 
-  key: fs.readFileSync("/ssl/STAR_courrio_com_key.txt"),
+  key: fs.readFileSync("./ssl/STAR_courrio_com_key.txt"),
 
-  cert: fs.readFileSync("/ssl/star.courrio.com.crt"),
+  cert: fs.readFileSync("./ssl/star.courrio.com.crt"),
 
 };
 
