9d1a806f0fd9ef767cbb19e50a0da059ca0f281a
Nur Akmal <2183322C@student.tp.edu.sg>
2022-02-24 22:56:04 +0800

    Update app.js

--- app.js
+++ app.js
@@ -447,7 +447,7 @@ router.post('/new_order', async (request, response) => {
 	
 			var deliveryDate;
 			try {
-				deliveryDate = computeDeliveryDate(rateCard["Delivery Type"], rateCard["Fixed Delivery Deadline"], rateCard["Order Cutoff"], rateCard["Delivery Deadline Home"], parseInt(rateCard["Days from Order to Delivery"]),deliveryDate);
+				deliveryDate = computeDeliveryDate(rateCard["Delivery Type"], rateCard["Fixed Delivery Deadline"], rateCard["Order Cutoff"], rateCard["Delivery Deadline Home"], parseInt(rateCard["Days from Order to Delivery"]),orderDate);
 			} catch (err) {
 				throw err;
 			}
