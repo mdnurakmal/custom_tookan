62bcf15035d761a131586233d25a3a2e08820e26
Nur Akmal <2183322C@student.tp.edu.sg>
2022-03-18 23:41:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -360,7 +360,6 @@ function checkIfNextDayIsWeekend(deliveryDate,deliveryDays,satDel,sunDel)
 {
 	var daysToAdd=0;
 	var tempDate = deliveryDate.clone();
-	console.log("delivery days " + deliveryDays);
 	for (let i = 0; i < deliveryDays; i++) 
 	{
 		var dayOfWeekBeforeDeliveryDays = tempDate.format('dddd');
@@ -412,8 +411,8 @@ function computeDeliveryDate(rate, fixedDeadline, orderCutOff, deliveryDeadline,
 	var isBefore = moment(orderDate.format("YYYY-MM-DD HH:mm:ss")).isBefore(cutoff);
 
 	if (isBefore) {
-
-		daysToDelivery+= checkIfNextDayIsWeekend(deliveryDate,daysToDelivery,satDel,sunDel);
+		deliveryDate = deliveryDate.add(1, "days");
+		daysToDelivery+= checkIfNextDayIsWeekend(deliveryDate,daysToDelivery,satDel,sunDel)-1;
 		deliveryDate = deliveryDate.add(daysToDelivery, "days");
 		console.log(deliveryDate.format("YYYY-MM-DD HH:mm:ss"));
 		console.log("Order placed before cut off time : Order is placed as next day")
@@ -422,10 +421,11 @@ function computeDeliveryDate(rate, fixedDeadline, orderCutOff, deliveryDeadline,
 
 		// add 1 day because its next day
 		if(checkIfAfterCutOffIsWeekend(deliveryDate,satDel,sunDel))
+			deliveryDate = deliveryDate.add(2, "days");
+		else
 			deliveryDate = deliveryDate.add(1, "days");
 
-
-		daysToDelivery+= checkIfNextDayIsWeekend(deliveryDate,daysToDelivery,satDel,sunDel);
+		daysToDelivery+= checkIfNextDayIsWeekend(deliveryDate,daysToDelivery,satDel,sunDel)-1;
 		deliveryDate = deliveryDate.add(daysToDelivery, "days");
 		console.log(deliveryDate.format("YYYY-MM-DD HH:mm:ss"));
 		console.log("Order placed after cut off time : Order is placed as next day")
